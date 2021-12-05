af7a4c8bef47e537716456d539a8c4bd3ee0c7f8
Jeremy Noesen <jeremynoesen@gmail.com>
2021-12-05 13:20:23 -0600

    fix formatting

--- show_image.py
+++ show_image.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""
+Draw an image of any size to the Unicorn HAT. Images will be scaled to fit a 1:1 aspect ratio. The scaling algorithm
+will divide the image up into an 8 by 8 grid, evenly among the x and y axes. The average color per grid block will be
+mapped directly to one LED on the matrix. To use, run the file with one argument, which will be the path to the image.
+"""
+
 import sys
 from PIL import Image
 import unicornhat as unicorn
@@ -10,30 +16,32 @@ unicorn.rotation(90)
 unicorn.brightness(0.5)
 
 # Get image and dimensions
-img = Image.open(str(sys.argv)[0])
-scaleX = int(img.size[0]/8)
-scaleY = int(img.size[1]/8)
+image = Image.open(str(sys.argv)[0])
+scale_x = int(image.size[0] / 8)
+scale_y = int(image.size[1] / 8)
 
 # Draw all pixels of matrix
-for matrixX in range(int(8)):
-    for matrixY in range(int(8)):
+for matrix_x in range(int(8)):
+    for matrix_y in range(int(8)):
 
-        r, g, b = 0, 0, 0
+        # Reset RGB values for next calculation cycle
+        r = 0
+        g = 0
+        b = 0
 
-        # Sum all colors in a block of pixels
-        for blockX in range(int(matrixX * scaleX), int((matrixX * scaleX) + scaleX - 1)):
-            for blockY in range(int(matrixY * scaleY), int((matrixY * scaleY) + scaleY - 1)):
-
-                pixel = img.getpixel((blockX, blockY))
+        # Sum all RGB values in a block of pixels
+        for block_x in range(int(matrix_x * scale_x), int((matrix_x * scale_x) + scale_x - 1)):
+            for block_y in range(int(matrix_y * scale_y), int((matrix_y * scale_y) + scale_y - 1)):
+                pixel = image.getpixel((block_x, block_y))
                 r += int(pixel[0])
                 g += int(pixel[1])
                 b += int(pixel[2])
 
-        # Average out the color of the block
-        r = int(r / (scaleX * scaleY))
-        g = int(g / (scaleX * scaleY))
-        b = int(b / (scaleX * scaleY))
+        # Get average RGB values for block
+        r = int(r / (scale_x * scale_y))
+        g = int(g / (scale_x * scale_y))
+        b = int(b / (scale_x * scale_y))
 
-        # Show pixel data on matrix
-        unicorn.set_pixel(matrixX, matrixY, r, g, b)
+        # Show pixel on matrix
+        unicorn.set_pixel(matrix_x, matrix_y, r, g, b)
         unicorn.show()
