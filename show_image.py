8789feae2b31dd2e5f5e042ce740198e66b0c52a
Jeremy Noesen <jeremynoesen@gmail.com>
2021-12-11 00:03:24 -0600

    add animation, looping, and processed frame caching (#1, #2, #3)

--- show_image.py
+++ show_image.py
@@ -9,6 +9,7 @@ Usage: sudo python show_image.py path/to/image
 """
 
 import sys
+import time
 from PIL import Image
 import unicornhat as unicorn
 
@@ -17,39 +18,68 @@ unicorn.set_layout(unicorn.HAT)
 unicorn.rotation(270)
 unicorn.brightness(1)
 
-# TODO add gif support
-
-# Load image, replacing alpha with black color
-raw = Image.open(str(sys.argv[1])).convert("RGBA")
-background = Image.new("RGBA", raw.size, (0, 0, 0))
-image = Image.alpha_composite(background, raw)
+# Load raw image
+raw = Image.open(str(sys.argv[1]))
 
 # Get scale factors for filtering
-scale_x = int(image.size[0] / 8)
-scale_y = int(image.size[1] / 8)
+width = raw.size[0]
+height = raw.size[1]
+scale_x = int(width / 8)
+scale_y = int(height / 8)
 
-# Draw all pixels of matrix
-for matrix_x in range(8):
-    for matrix_y in range(8):
+# Get animation data
+frames = getattr(raw, "n_frames", 1)
+if getattr(raw, "is_animated", False) is True:
+    frame_step = raw.info['duration'] / float(frames)
+else:
+    frame_step = 60
 
-        # Reset RGB values for next calculation cycle
-        r = 0
-        g = 0
-        b = 0
+# Process all frames of image before displaying
+processed_frames = [[[(0, 0, 0)] * width for i in range(height)] for j in range(frames)]
+for i in range(frames):
+    raw.seek(i)
+    frame = raw.convert("RGBA")
+    background = Image.new("RGBA", raw.size, (0, 0, 0))
+    image = Image.alpha_composite(background, frame)
+    for matrix_x in range(8):
+        for matrix_y in range(8):
 
-        # Sum all RGB values in a block of pixels
-        for block_x in range(matrix_x * scale_x, (matrix_x * scale_x) + scale_x):
-            for block_y in range(matrix_y * scale_y, (matrix_y * scale_y) + scale_y):
-                pixel = image.getpixel((block_x, block_y))
-                r += int(pixel[0])
-                g += int(pixel[1])
-                b += int(pixel[2])
+            # Reset RGB values for next calculation cycle
+            r = 0
+            g = 0
+            b = 0
 
-        # Get average RGB values for block
-        r = int(r / (scale_x * scale_y))
-        g = int(g / (scale_x * scale_y))
-        b = int(b / (scale_x * scale_y))
+            # Sum all RGB values in a block of pixels
+            for block_x in range(matrix_x * scale_x, (matrix_x * scale_x) + scale_x):
+                for block_y in range(matrix_y * scale_y, (matrix_y * scale_y) + scale_y):
+                    pixel = image.getpixel((block_x, block_y))
+                    r += int(pixel[0])
+                    g += int(pixel[1])
+                    b += int(pixel[2])
 
-        # Show pixel on matrix
-        unicorn.set_pixel(matrix_x, matrix_y, r, g, b)
-unicorn.show()
+            # Get average RGB values for block
+            r = int(r / (scale_x * scale_y))
+            g = int(g / (scale_x * scale_y))
+            b = int(b / (scale_x * scale_y))
+
+            # Store processed image
+            processed_frames[i][matrix_x][matrix_y] = (r, g, b)
+
+# Display frames of image on a loop
+current_frame_index = 0
+while True:
+    current_frame = processed_frames[current_frame_index]
+
+    # Display image
+    for matrix_x in range(width):
+        for matrix_y in range(height):
+            pixel = current_frame[matrix_x][matrix_y]
+            unicorn.set_pixel(matrix_x, matrix_y, pixel)
+    unicorn.show()
+
+    # Wait for next frame
+    if current_frame_index is frames - 1:
+        current_frame_index = 0
+    else:
+        current_frame_index += 1
+    time.sleep(frame_step)
