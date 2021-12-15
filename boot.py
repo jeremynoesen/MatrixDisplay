fd1e75952fdc266c4c6a860f971cec5d1574bc19
Jeremy Noesen <jeremynoesen@gmail.com>
2021-12-15 15:00:42 -0600

    simplify boot animation code

--- boot.py
+++ boot.py
@@ -21,25 +21,20 @@ pixels = [(7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7),
           (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0)]
 
 # Do pixel animation
-j = 0
 for j in range(len(pixels)):
     pixel = pixels[j]
-    j += 1
     unicorn.set_pixel(pixel[0], pixel[1], 255, 255, 255)
     unicorn.show()
     time.sleep(0.07 - (j * 0.002))
 
 # Fade in
-i = 0.5
-while i < 1.0:
-    unicorn.brightness(i)
+for i in range(50, 101):
+    unicorn.brightness(i / 100.0)
     unicorn.show()
-    i = min(i + 0.05, 1.0)
-    time.sleep(0.01)
+    time.sleep(0.002)
 
 # Fade out
-while i > 0.0:
-    unicorn.brightness(i)
+for i in range(0, 76):
+    unicorn.brightness(1 - (i / 100.0))
     unicorn.show()
-    i = max(i - 0.008, 0.0)
     time.sleep(0.01)
