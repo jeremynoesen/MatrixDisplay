6a66c43dfe49c0c1b4ffc31ff408442f111d57d7
Jeremy Noesen <jeremynoesen@gmail.com>
2021-12-21 13:16:29 -0600

    add filter intensity

--- filter.py
+++ filter.py
@@ -5,6 +5,7 @@ Simple blue light filter for the Unicorn HAT
 import unicornhat as unicorn
 
 filter_enabled = False
+filter_intensity = 30
 
 
 def set_pixel(x, y, r, g, b):
@@ -16,16 +17,25 @@ def set_pixel(x, y, r, g, b):
     :param g: green value
     :param b: blue value
     """
-    if filter_enabled:
-        unicorn.set_pixel(x, y, r, max(g - 50, 0), max(b - 100, 0))
+    if filter_enabled is True:
+        unicorn.set_pixel(x, y, r, max(g - filter_intensity, 0), max(b - (filter_intensity * 3), 0))
     else:
         unicorn.set_pixel(x, y, r, g, b)
 
 
-def enable_filter(enabled):
+def set_filter_enabled(enabled):
     """
     Enable or disable the blue light filter
     :param enabled: True to enable filter
     """
     global filter_enabled
     filter_enabled = enabled
+
+
+def set_filter_intensity(intensity):
+    """
+    Set filter intensity
+    :param intensity: 0 to 100 intensity of filter
+    """
+    global filter_intensity
+    filter_intensity = intensity
