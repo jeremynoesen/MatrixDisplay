429c07468770269eaa85d6d07d20b1df860f4ad8
Jeremy Noesen <jeremynoesen@gmail.com>
2021-12-21 11:57:27 -0600

    comments

--- filter.py
+++ filter.py
@@ -8,6 +8,14 @@ filter_enabled = False
 
 
 def set_pixel(x, y, r, g, b):
+    """
+    Set the color of a pixel with or without the blue light filter
+    :param x: Matrix pixel x coordinate
+    :param y: Matrix pixel y coordinate
+    :param r: Red value
+    :param g: green value
+    :param b: blue value
+    """
     if filter_enabled:
         unicorn.set_pixel(x, y, r, max(g - 50, 0), max(b - 100, 0))
     else:
@@ -15,4 +23,8 @@ def set_pixel(x, y, r, g, b):
 
 
 def enable_filter(enabled):
+    """
+    Enable or disable the blue light filter
+    :param enabled: True to enable filter
+    """
     filter.filter_enabled = enabled
