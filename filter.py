cc435cd43e5074ff96edad0ea5d3380702d898d9
Jeremy Noesen <jeremynoesen@gmail.com>
2021-12-22 20:22:17 -0600

    added getters

--- filter.py
+++ filter.py
@@ -32,6 +32,14 @@ def set_warmth(warmth):
     current_warmth = min(max(warmth, 0), 100)
 
 
+def get_warmth():
+    """
+    Get the warmth of the display
+    :return: Warmth of the display 0 to 100
+    """
+    return current_warmth
+
+
 def set_brightness(brightness):
     """
     Set software display brightness
@@ -39,3 +47,11 @@ def set_brightness(brightness):
     """
     global current_brightness
     current_brightness = min(max(brightness, 0), 100) / 100.0
+
+
+def get_brightness():
+    """
+    Get the software display brightness
+    :return: Brightness of display 0 to 100
+    """
+    return int(current_brightness * 100)
