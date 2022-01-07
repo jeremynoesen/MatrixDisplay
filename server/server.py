0cf3fe19c1512c1bd665861936908f8f48a31dfb
Jeremy Noesen <jeremynoesen@gmail.com>
2022-01-07 10:00:09 -0600

    refactoring

--- server/server.py
+++ server/server.py
@@ -39,10 +39,10 @@ class Server(BaseHTTPRequestHandler):
         # Send the HTML over to create the web page
         with open("./server/index.html") as fd:
             html = fd.read().replace("{links_str}", links_str) \
-                .replace("{brightness}", str(display.get_brightness())) \
-                .replace("{warmth}", str(display.current_warmth)) \
                 .replace("{duration}", str(slideshow.display_time)) \
-                .replace("{color}", color.current_color)
+                .replace("{color}", color.current_color) \
+                .replace("{brightness}", str(display.get_brightness())) \
+                .replace("{warmth}", str(display.current_warmth))
             self.do_HEAD()
             self.wfile.write(html.encode("utf-8"))
 
@@ -51,28 +51,28 @@ class Server(BaseHTTPRequestHandler):
             file = self.path.replace("/image/", "")
             display.clear()
             image.show(Image.open(f"{pictures_dir}/{file}"), True)
+        elif self.path == "/slideshow":
+            display.clear()
+            slideshow.show(pictures_dir)
+        elif self.path.startswith("/slideshow/"):
+            display_time = int(self.path.replace("/slideshow/", ""))
+            slideshow.set_display_time(display_time)
+        elif self.path == "/color":
+            display.clear()
+            color.show()
+        elif self.path.startswith("/color/"):
+            color.current_color = self.path.replace("/color/", "")
+        elif self.path == "/off":
+            display.clear()
         elif self.path.startswith("/brightness/"):
             brightness = int(self.path.replace("/brightness/", ""))
             display.set_brightness(brightness)
         elif self.path.startswith("/warmth/"):
             warmth = int(self.path.replace("/warmth/", ""))
             display.set_warmth(warmth)
-        elif self.path.startswith("/slideshow/"):
-            display_time = int(self.path.replace("/slideshow/", ""))
-            slideshow.set_display_time(display_time)
-        elif self.path == "/slideshow":
-            display.clear()
-            slideshow.show(pictures_dir)
-        elif self.path == "/off":
-            display.clear()
         elif self.path == "/update":
             display.clear()
             os.system("git pull; sudo reboot")
-        elif self.path.startswith("/color/"):
-            color.current_color = self.path.replace("/color/", "")
-        elif self.path == "/color":
-            display.clear()
-            color.show()
 
 
 def start():
