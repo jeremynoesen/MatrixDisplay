f1e0f7c23a8fd4064040e29d30915a332270ff1c
Jeremy Noesen <jeremynoesen@gmail.com>
2022-01-11 14:16:32 -0600

    up refresh rate

--- graphics/color.py
+++ graphics/color.py
@@ -30,7 +30,7 @@ def __show():
             for j in range(8):
                 display.set_pixel(i, j, color[0], color[1], color[2])
         unicorn.show()
-        time.sleep(0.0333)
+        time.sleep(0.02)
 
 
 def show():
