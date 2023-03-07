acfa7a7a5d4ea813098eb07ba8584e2727529fc9
Jeremy Noesen <jeremynoesen@gmail.com>
2023-03-07 12:49:11 -0600

    404 handling

--- server/server.py
+++ server/server.py
@@ -99,6 +99,10 @@ class Server(BaseHTTPRequestHandler):
                 "warmth": display.current_warmth
             }
             self.wfile.write(json.dumps(data).encode("utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
 
     def do_POST(self):
         """
@@ -154,6 +158,10 @@ class Server(BaseHTTPRequestHandler):
             except TypeError:
                 current_mode = "off"
                 display.clear()
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
 
 
 def start():
